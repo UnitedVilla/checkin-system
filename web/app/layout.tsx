f48fef42e12c9696e2f14de66be5b8bd180070e2
yuki terauchi <y.terauchi107@gmail.com>
2025-08-16 20:02:30 +0900

    2002

--- web/app/layout.tsx
+++
@@ -1,2 +0,0 @@
-export const metadata = { title: 'オンラインチェックイン', description: '和モダンのオンラインチェックイン' };
-export default function RootLayout({children}:{children:React.ReactNode}){return(<html lang='ja'><body className='min-h-screen bg-slate-50'>{children}</body></html>)}
